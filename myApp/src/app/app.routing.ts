7497492a6fa14839132e8f89c6300989079bd04a
DESKTOP-G5EVJQL\nadav <nadavmor123@gmail.com>
2019-05-06 17:26:03 +0300

    init app routing + lazy loading modules

---
+++ myApp/src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {RouterModule ,Routes} from '@angular/router';
+import {AppComponent} from './app.component';
+
+const appRoutes : Routes = [
+    {
+        path: 'movies',
+        loadChildren: './movies/movies.module#MoviesModule'
+      }, 
+      {
+        path: '',
+        redirectTo: '',
+        pathMatch: 'full'
+      }
+];
+
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(
+            appRoutes
+        )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule{}
