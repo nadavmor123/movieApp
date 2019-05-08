a33ebc75242a23aceb25cbce8d2fd24086d4ef81
DESKTOP-G5EVJQL\nadav <nadavmor123@gmail.com>
2019-05-08 14:17:40 +0300

    poster images for list

--- myApp/src/app/movies/services/api.service.ts
+++ myApp/src/app/movies/services/api.service.ts
@@ -14,24 +14,29 @@ export class ApiService {
   constructor(private httpClient: HttpClient) {
 
     this.movieList = [
-      {Id:1,Title:"The Avengers"},
-      {Id:2,Title:"Captain America: The Winter Soldier"},
-      {Id:3,Title:"Captain America: The First Avenger"},
+      {Id:1,Title:"Spiderman: Homecoming"},
+      {Id:2,Title:"Captain America: The First Avenger"},
+      {Id:3,Title:"Captain America: The Winter Soldier"},
       {Id:4,Title:"Captain America: Civil War"},
-      {Id:5,Title:"Avengers: Endgame"},
+      {Id:5,Title:"The Avengers"},
       {Id:6,Title:"Avengers: Age Of Ultron"},
       {Id:7,Title:"Avengers: Infinty War"},
       {Id:8,Title:"Avengers: Endgame"},
       {Id:9,Title:"Iron Man"},
       {Id:10,Title:"Iron Man 2"},
       {Id:11,Title:"Iron Man 3"},
-      {Id:12,Title:"Gardians Of The Galaxy"},
-      {Id:13,Title:"Gardians Of The Galaxy vol. 2"},
+      {Id:13,Title:"Gardians Of The Galaxy"},
+      {Id:12,Title:"Gardians Of The Galaxy vol. 2"},
       {Id:14,Title:"Thor"},
       {Id:15,Title:"Thor: The Dark World"},
       {Id:16,Title:"Thor: Ragnarok"},
       {Id:17,Title:"Doctor Strange"},
       {Id:18,Title:"Captain Marvel"},
+      {Id:19,Title:"Black Panther"},
+      {Id:20,Title:"Ant-Man"},
+      {Id:21,Title:"Ant-Man and the Wasp"},
+      {Id:22,Title:"Deadpool"},
+      {Id:23,Title:"Deadpool 2"},
     ]
   }
 
